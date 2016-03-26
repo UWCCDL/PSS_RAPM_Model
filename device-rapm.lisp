493eb3f2ab997404be352be09575b090018645b2
ANDREA STOCCO <stocco@uw.edu>
2016-03-26 11:57:02 -0700

    Almost completed new RAPM device interface
    More in line with my previous interfaces. Allows for realistic flow.

--- device-rapm.lisp
+++ device-rapm.lisp
@@ -5,6 +5,8 @@
 ;;; version of Raven's Advanced Progressive Matrices
 ;;; ------------------------------------------------------------------
 
+(load "rapm-problems.lisp")
+
 (defun act-r-loaded? ()
   "Cheap hack to check whether ACTR is loaded"
   (and (fboundp 'run-n-events)
@@ -18,7 +20,6 @@
   "Picks up an element from a list"
   (when  (listp lst)
     (elt lst (random (length lst)))))
-
 
 (defun scramble (lst &optional (sofar nil))
   "Scrambles a list of different elements"
@@ -40,39 +41,18 @@
     (/ (reduce #'+ nums)
        (length nums))))
 
-
-;;; Definition of a problem in RAPM
-
-(setq problem
-      '(((number 1 shape triangle)
-	 (number 2 shape triangle)
-	 (number 3 shape triangle))
-	
-	((number 1 shape square)
-	 (number 2 shape square)
-	 (number 3 shape square))
-	
-	((number 1 shape circle)
-	 (number 2 shape circle)
-	 nil)))
-
-(setq simple-problem
-      '(((shape triangle)
-	 (shape triangle)
-	 (shape triangle))
-	
-	((shape triangle)
-	 (shape triangle)
-	 (shape triangle))
-	
-	((shape triangle)
-	 (shape triangle)
-	 nil)))
+;;; ------------------------------------------------------------------
+;;; PARAMETERS
+;;; ------------------------------------------------------------------
 
 (defparameter *number-names* '((0 . zero) (1 . one) (2 . two)
 			       (3 . three) (4 . four) (5 . five)
 			       (6 . six) (7 . seven) (8 . eight)
-			       (9 . nine) (10 . ten)))
+			       (9 . nine) (10 . ten))
+  "Maps numbers onto their symbolic names")
+
+(defparameter *responses* '(("j" . 0) ("k" . 1) ("l" . 2) (";" . 3))
+  "Maps finger responses onto screen options")
 
       
 (defun convert-to-number (token)
@@ -91,8 +71,12 @@
 	(push (cons problem id) *pid*)
 	id)))
   
+;;; ------------------------------------------------------------------
+;;; CELLS AND PROBLEMS: PROCESSING AND DEFINITION
+;;; ------------------------------------------------------------------
 
 (defun divide-into-pairs (lst &optional (partial nil) (open nil))
+  "Recursively divides a list into pairs"
   (cond ((null lst)
 	 (append partial open))
 	((= (length (car open)) 2)
@@ -111,6 +95,7 @@
 	  
 
 (defun same-elements (lst1 lst2)
+  "Quickly determines whether two lists contains the same elements (barring repetitions)"
   (and (= (length lst1) (length lst2))
        (every #'(lambda (x) (member x lst2 :test #'equalp)) lst1)
        (every #'(lambda (x) (member x lst1 :test #'equalp)) lst2)))
@@ -121,6 +106,7 @@
        (every #'atom c)))
 
 (defun equal-cell (c1 c2)
+  "Equality for RAPM cells"
   (and (valid-cell? c1)
        (valid-cell? c2)
        (same-elements (divide-into-pairs c1)
@@ -134,13 +120,14 @@
        (< y 3)))
 
 (defun valid-problem? (p)
-  "Tests whether a problem is valid"
+  "A problem is valid if it is made of three rows of cells"
   (and (= 3 (length p))
        (every #'(lambda (x) (and (= 3 (length x))
 				 (every #'valid-cell? x)))
 	      p)))
 
 (defun equal-row (row1 row2)
+  "Two rows are equal if their cells are equal"
   (every #'equal-cell row1 row2))
 
 (defun equal-problem (p1 p2)
@@ -151,45 +138,81 @@
 
 
 (defun problem-cell (problem row col)
+  "Returns the cell in position ROW/COL"
   (when (and (valid-coordinates? row col)
 	     (valid-problem? problem))
     (nth col (nth row problem))))
 
 
-(setf trial
-      (list problem '(number 3 shape circle)
-	    '((number 4 shape cirle)
-	      (number 3 shape circle)
-	      (number 6 shape triangle)
-	      (number 2 shape diamond))))
+;;; ------------------------------------------------------------------
+;;; EXPERIMENTAL TRIALS
+;;; ------------------------------------------------------------------
+;;; A trial is a list containing  a problem, a solution, a list of
+;;; options, and a set of optional values to measure accuracy and RTs.
+;;; ------------------------------------------------------------------
 
-(setf simple-trial
-      (list simple-problem
-	    '(shape triangle)
-	    '((shape circle)
-	      (shape square)
-	      (shape triangle)
-	      (shape diamond))))
+;;; Checks whether a trial is valid
 
+(defun valid-trial? (trl)
+  "A trial is valid is it is made of a valid problem, a valid cell (solution), and a list of valid options" 
+  (and (>= (length trl) 3)
+       (valid-problem? (first trl))
+       (valid-cell? (second trl))
+       (valid-options? (third trl))))
+
+
+(defun make-trial (trl)
+  "Generates a fully useable trial from a three-item list"
+  (let* ((new-options (scramble* (trial-options trl)))
+	 (correct (position (trial-solution trl)
+			    new-options
+			    :test #'equal-cell)))
+    (list (trial-problem trl)              ; Problem
+	  (trial-solution trl)             ; Solution
+	  new-options                      ; Options
+	  correct                          ; Correct response
+	  nil                              ; Actual response
+	  0                                ; Problem onset
+	  0                                ; Problem offset
+	  0                                ; Choice onset
+	  0)))                             ; Choice response time
+
+
+;;; The trial problem
 
 (defun trial-problem (trl)
   "Returns the problem in a RAPM trial"
-  (first trl))
+  (nth 0 trl))
 
+(defun set-trial-problem (trl p)
+  "Returns the problem in a RAPM trial"
+  (setf (nth 0 trl) p))
+
+;;; The trial solution
 
 (defun trial-solution (trl)
   "Returns the solution of problem in a RAPM trial"
-  (second trl))
+  (nth 1 trl))
 
+(defun set-trial-solution (trl sol)
+  "Returns the solution of problem in a RAPM trial"
+  (setf (nth 1 trl) sol))
+
+;;; The trial's list of options to choose from
 
 (defun trial-options (trl)
   "Returns the response options available for a trial" 
-  (third trl))
+  (nth 2 trl))
 
-  
+(defun trial-options (trl opts)
+  "Returns the response options available for a trial" 
+  (setf (nth 2 trl) opts))
+
 (defun valid-options? (opt)
   "A list of options is valid if every member is a valid cell" 
   (every #'valid-cell opt))
+
+;;; The correct response
 
 (defun trial-correct-response (trl)
   "Returns the correct answer of a trial"
@@ -199,6 +222,7 @@
   "Returns the correct answer of a trial"
   (setf (nth 3 trl) val))
 
+;;; The actual responses
 
 (defun trial-actual-response (trl)
   "Returns the answer given by the model"
@@ -208,47 +232,61 @@
   "Returns the answer given by the model"
   (setf (nth 4 trl) val))
 
+;;; The problem onset
 
-(defun trial-problem-rt (trl)
+(defun trial-problem-onset (trl)
   (nth 5 trl))
 
-(defun set-trial-problem-rt (trl val)
+(defun set-trial-problem-onset (trl val)
   (setf (nth 5 trl) val))
 
+;;; The problem response time
 
-(defun trial-choice-rt (trl)
+(defun trial-problem-response-time (trl)
   (nth 6 trl))
 
-(defun set-trial-choice-rt (trl val)
+(defun set-trial-problem-response-time (trl val)
   (setf (nth 6 trl) val))
 
 
-(defun valid-trial? (trl)
-  "A trial is valid is it is made of a valid problem, a valid cell (solution), and a list of valid options" 
-  (and (>= (length trl) 3)
-       (valid-problem? (first trl))
-       (valid-cell? (second trl))
-       (valid-options? (third trl))))
+;;; The choice onset
 
-(defparameter *responses* '(("j" . 0) ("k" . 1) ("l" . 2) (";" . 3)))
+(defun trial-choice-onset (trl)
+  (nth 7 trl))
 
-(defun make-trial (trl)
-  (let* ((new-options (scramble* (trial-options trl)))
-	 (correct (position (trial-solution trl) new-options)))
-    (list (trial-problem trl)              ; Problem
-	  (trial-solution trl)             ; Solution
-	  new-options                      ; Options
-	  correct                          ; Correct response
-	  nil                              ; Actual response
-	  0                                ; Problem RT
-	  0)))                             ; Choice RT
+(defun set-trial-choice-onset (trl val)
+  (setf (nth 7 trl) val))
+
+
+;;; The choice response time
+
+(defun trial-choice-response-time (trl)
+  (nth 8 trl))
+
+(defun set-trial-choice-onset (trl val)
+  (setf (nth 8 trl) val))
+
+
+;;; Accuracy and RTs
 
 (defun trial-accuracy (trl)
+  "Returns 1 if the choice was correct, 0 otherwise" 
   (if (and (>= (length trl) 7)
 	   (= (trial-correct-response trl)
 	      (trial-actual-response trl)))
       1
       0))
+
+(defun trial-problem-rt (trl)
+  "Calculates the problem's RT"
+  (- (trial-problem-response-time trl)
+     (trial-problem-onset trl)))
+
+(defun trial-chocie-rt (trl)
+  "Calculates the problem's RT"
+  (- (trial-choice-response-time trl)
+     (trial-choice-onset trl)))
+
 
 
 ;;; ------------------------------------------------------------------
@@ -281,39 +319,60 @@
     (setf (current-trial task) (make-trial (nth (index task) (trials task))))
     (setf (task-phase task) 'problem)))
 
-(defmethod respond ((task rapm-task) key)
+(defparameter *phase-transitions* '((problem . pause1) (pause1 . choice)
+				    (choice . pause2) (pause2 . problem)))
+
+(defun get-next-phase (phase)
+  (cdr assoc phase *phase-transitions*))
+
+(defmethod next ((task rapm-task))
+  "Moves on to the next stage of the task"
+  (unless (null (index task))  ; If it nil, the tast is not initialized yetr
+    (incf (index task))  ; Increament the index. This is easy
+    (push (current-trial task) (experiment-log task))
+    (let* ((current-phase (task-phase task))
+	   (next-phase (get-next-phase current-phase)))
+      (if  (and (equal current-phase 'choice)
+		(>= (index task) (length (trials task))))
+	   (setf (task-phase task) 'done)
+	   (setf (task-phase task) next-phase))
+      (when (act-r-loaded?)
+	(schedule-event-relative 0 #'proc-display :params nil)
+	(when (member next-phase '(pause1 pause2))
+	  (schedule-event-relative 1 #'next :params (list task)))))))
+	      
+	   
+		
+
+
+
+(defmethod respond ((task rapm-task) response)
   "Records a response in the PSS task"
   (unless (null (current-trial task))
-    (let* ((trial (current-trial task))
-	   (choice (trial-choice trial))
-	   (chosen (nth (cdr (assoc key *key-mappings*))
-			choice))
-	   (n (random 1.0))
-	   (prob (cdr (assoc chosen *probabilities*)))
-	   
-	   (feedback (< n prob)))
-
-      (set-trial-chosen-option trial chosen)
-      (set-trial-best-option trial (trial-best-option trial))
-      (set-trial-feedback trial feedback))
-
-    ;; If ACT-R is loaded, we need to sync the visicon with the
-    ;; state of the task.
+    (let* ((trial (current-trial task)))
+      (set-trial-actual-response trial response))
+      
+    ;; If ACT-R is loaded, we need to record response times
+    ;; and sync the visicon 
 
     (when (act-r-loaded?)
-      (cond ((equal (phase task) 'test)
-	     (schedule-event-relative 0 #'next :params (list task)))
-	    ((equal (phase task) 'training)
-	     (schedule-event-relative 0 #'proc-display :params nil)
-	     (schedule-event-relative 3 #'next :params (list task)))
-	    (t
-	     (schedule-event-relative 0 #'proc-display :params nil))))))
+      (let ((tme (mp-time)))
+	(cond ((equal (phase task) 'problem)
+	       (set-trial-problem-response-time tme))
+	      ((equal (phase task) 'choice)
+	       (set-trial-problem-choice-time tme)))
+	(schedule-event-relative 0 #'next :params (list task))))))
 
+(defmethod next ((task rapm-task))
+  "Advances to the next stage"
+  ())
 
 (defmethod device-handle-keypress ((tm list) key)
   "Converts the key into a symbol and passes it on to the task manager"
-  (let ((val (read-from-string (format nil "~a" key))))
-    (record-response tm val)))
+  (let ((val (cdr (assoc (format nil "~a" key)
+			 *responses
+			 :test #'string-equal))))
+    (respond tm val)))
 			   
 (defmethod device-handle-click ((device list))
   "Does nothing"
---
+++ rapm-problems.lisp
@@ -0,0 +1,48 @@
+;;; ------------------------------------------------------------------
+;;; RAPM PROBLEMS
+;;; ------------------------------------------------------------------
+;;; Lisp definition of the RAPM problems used by Lauren Graham
+;;; ------------------------------------------------------------------
+
+(setq problem
+      '(((number 1 shape triangle)
+	 (number 2 shape triangle)
+	 (number 3 shape triangle))
+	
+	((number 1 shape square)
+	 (number 2 shape square)
+	 (number 3 shape square))
+	
+	((number 1 shape circle)
+	 (number 2 shape circle)
+	 nil)))
+
+(setq simple-problem
+      '(((shape triangle)
+	 (shape triangle)
+	 (shape triangle))
+	
+	((shape triangle)
+	 (shape triangle)
+	 (shape triangle))
+	
+	((shape triangle)
+	 (shape triangle)
+	 nil)))
+
+(setf trial
+      (list problem '(number 3 shape circle)
+	    '((number 4 shape cirle)
+	      (number 3 shape circle)
+	      (number 6 shape triangle)
+	      (number 2 shape diamond))))
+
+(setf simple-trial
+      (list simple-problem
+	    '(shape triangle)
+	    '((shape circle)
+	      (shape square)
+	      (shape triangle)
+	      (shape diamond))))
+
+(setf *trials* (list simple-trial))
