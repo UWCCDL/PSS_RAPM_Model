83b09c74b268665b9941b1411035f399027c2e7e
gardoma <andrea.stocco@gmail.com>
2017-10-09 22:23:26 -0700

    Improved test

--- devel4/test.lisp
+++ devel4/test.lisp
@@ -1,6 +1,6 @@
 (load "/projects/actr/actr7/load-act-r.lisp")
 (load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-device.lisp")
-(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-model.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-model-newchoice.lisp")
 (load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-simulations.lisp")
-(general-simulations 200 :fname "simulations-devel4-model2-bold.txt") ;; :tickvals '(20))
+(general-simulations 200 :fname "simulations-devel4-newchoice-bold.txt") ;; :tickvals '(20))
 (quit)
