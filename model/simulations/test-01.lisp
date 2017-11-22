b79cd40f54c11d99a782abae813d67aaf472288f
TheRealDrDre <stocco@uw.edu>
2017-11-21 23:28:01 -0800

    Cleaned up and reorganized code

---
+++ model/simulations/test-01.lisp
@@ -0,0 +1,7 @@
+
+(load "/projects/actr/actr7/load-act-r.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-device.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-model-newchoice.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-simulations.lisp")
+(general-simulations 200 :fname "simulations-devel4-newchoice-newprobs-newbold-tick-20-upper-1.0.txt" :tickvals '(20) :upprbndvals '(1.0))
+(quit)
---
+++ model/simulations/test-03bis.lisp
@@ -0,0 +1,7 @@
+
+(load "/projects/actr/actr7/load-act-r.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-device.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-model-newchoice.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-simulations.lisp")
+(general-simulations 200 :fname "simulations-devel4-newchoice-newprobs-newbold-tick-22-upper-3.0.txt" :tickvals '(22) :upprbndvals '(3.0))
+(quit)
