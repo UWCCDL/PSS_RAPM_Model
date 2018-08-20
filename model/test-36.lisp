e703070e23d98b1664d1d14b9b5fafea820f635d
TheRealDrDre <stocco@uw.edu>
2018-08-19 17:38:38 -0700

    Recording difficulty level in simulation code

--- model/test-36.lisp
+++ model/test-36.lisp
@@ -1,7 +1,7 @@
 
 (load "/projects/actr/actr7/load-act-r.lisp")
-(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-device.lisp")
-(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-model-newchoice.lisp")
-(load "/projects/actr/models/PSS_RAPM_Model/devel4/rapm-simulations.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/rapm-device.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/rapm-model.lisp")
+(load "/projects/actr/models/PSS_RAPM_Model/rapm-simulations.lisp")
 (general-simulations 200 :fname "simulations-devel4-newchoice-newprobs-newbold-tick-25-upper-3.0-difficulty2.txt" :tickvals '(25) :upprbndvals '(3.0) :difficulty '(2))
 (quit)
